86896593a766f79f13dbdcd736e5161874f81e8b
Georgi Fotev <georgifotev12@gmail.com>
2022-09-25 22:09:41 +0300

    updating app

--- src/App.js
+++ src/App.js
@@ -5,23 +5,26 @@ import HomeView from "./views/HomeView";
 import PostsView from "./views/PostsView";
 import UsersView from "./views/UsersView";
 import AddUserView from "./views/addUserView";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className='App'>
         <h1>My simple app</h1>
-        <ul className='header'>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/posts'>Posts</Link>
-          </li>
-          <li>
-            <Link to='/users'>Users</Link>
-          </li>
-        </ul>
+        <nav>
+          <ul className='header'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/posts'>Posts</Link>
+            </li>
+            <li>
+              <Link to='/users'>Users</Link>
+            </li>
+          </ul>
+        </nav>
         <div className='content'>
           <Routes>
             <Route path='/' element={<HomeView />} />
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,30 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function HomeView() {
-  const [search, setSearch] = useState("");
-  const [data, setData] = useState(null);
+  const [choise, setChoise] = useState("users");
+  let [data, setData] = useState([]);
+  const path = "/" + choise;
+  let matches = [];
 
   const submitHandler = (ev) => {
     ev.preventDefault();
+    const searchFor = document.querySelector("input").value;
+    axios
+      .get("http://localhost:3030" + path)
+      .then((response) => {
+        for (let token of response.data) {
+          if (choise == "posts") {
+            if (token.title.includes(searchFor)) {
+              matches.push(token.title);
+            }
+          } else {
+            if (token.username.includes(searchFor)) {
+              matches.push(token.username);
+            }
+          }
+        }
+        setData(matches);
+        document.querySelector("input").value = "";
+      })
+      .catch((err) => console.log(err));
   };
+
   return (
     <form onSubmit={submitHandler}>
       <div>
         <h2>Welcome to my simple app!</h2>
         <p>Search:</p>
-        <input
-          type='text'
-          name='search'
-          value={search}
-          onChange={(ev) => setSearch(ev.target.value)}
-        />
-        <select>
-          <option>Post</option>
-          <option>User</option>
+        <input type='text' placeholder='username or title' />
+        <select value={choise} onChange={(ev) => setChoise(ev.target.value)}>
+          <option value='posts'>Post title</option>
+          <option value='users'>Username</option>
         </select>
         <button type='submit'>Search</button>
-        <div className='searchContent'></div>
+        <div className='searchContent'>
+          <ol>
+            {data.length
+              ? data.map((input) => <li key={input}>{input}</li>)
+              : null}
+          </ol>
+        </div>
       </div>
     </form>
   );
